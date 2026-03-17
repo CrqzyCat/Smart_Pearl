0e5a260893c2681931b91e05145b198ce3c0dc72
crqzycat <crqzycat@gmail.com>
2026-03-17 17:46:56 +0100

    Initial commit

---
+++ src/client/java/me/smart_pearl/smart_pearl/client/Smart_pearlClient.java
@@ -0,0 +1,10 @@
+package me.smart_pearl.smart_pearl.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Smart_pearlClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/smart_pearl/smart_pearl/Smart_pearl.java
@@ -0,0 +1,10 @@
+package me.smart_pearl.smart_pearl;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Smart_pearl implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
